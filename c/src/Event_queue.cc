4f03df9be10c2b4b369a2d30e9ae902018d178d2
Katya Katsenelenbogen <katyak@mellanox.com>
2013-08-28 13:28:15 +0300

    modifying name to underscore

---
+++ c/src/Event_queue.cc
@@ -0,0 +1,67 @@
+/*
+** Copyright (C) 2013 Mellanox Technologies
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at:
+**
+** http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+** either express or implied. See the License for the specific language
+** governing permissions and  limitations under the License.
+**
+*/
+#include <string.h>
+#include <map>
+
+
+#include "Event_queue.h"
+
+Event_queue::Event_queue(int size)
+{
+	error_creating = false;
+	this->buf = (char*)malloc(size * sizeof(char));
+	if (this->buf== NULL){
+		log(lsERROR, "Could not allocate memory for Event Queue buffer\n");
+		error_creating = true;
+		return;
+	}
+
+	this->offset = 0;
+	this->size = size;
+}
+
+
+Event_queue::~Event_queue()
+{
+	if (this->buf!= NULL){
+		free(this->buf);
+	}
+}
+
+void Event_queue::reset()
+{
+	this->offset = 0;
+}
+
+char* Event_queue::get_buffer()
+{
+	return this->buf + this->offset;
+}
+
+void Event_queue::increase_offset(int increase)
+{
+	this->offset += increase;
+}
+
+
+
+
+
+
+
+
+
---
+++ c/src/Event_queue.h
@@ -0,0 +1,50 @@
+/*
+** Copyright (C) 2013 Mellanox Technologies
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at:
+**
+** http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+** either express or implied. See the License for the specific language
+** governing permissions and  limitations under the License.
+**
+*/
+
+
+#ifndef Event_queue__H___
+#define Event_queue__H___
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "Utils.h"
+
+//TODO:: check for overflow of the buffer
+class Event_queue{
+
+
+public:
+	Event_queue(int size);
+	~Event_queue();
+	char* get_buffer();
+	void reset();
+	void increase_offset(int increase);
+
+	bool error_creating;
+
+private:
+	int offset;
+	char * buf;
+	int size;
+
+
+};
+
+
+
+
+#endif // ! Event_queue__H___
