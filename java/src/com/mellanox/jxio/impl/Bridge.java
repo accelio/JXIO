39ab04076446a8e704c73d8c96935ced13c5ee7b
Alex Rosenbaum <Alexr@mellanox.com>
2013-10-01 16:59:25 +0300

    Lower the Bridge log level from INFO to FINE
    Change-Id: I80e9a711297b7da572864fb277dc3c738567326e

--- java/src/com/mellanox/jxio/impl/Bridge.java
+++ java/src/com/mellanox/jxio/impl/Bridge.java
@@ -33,99 +33,99 @@ public class Bridge {
 	
 	private static native boolean createCtxNative(int eventQueueSize, Object dataFromC);
 	public static boolean createCtx(int eventQueueSize, Object dataFromC) {
-		logger.log(Level.INFO, "invoking createCtxNative");
+		logger.log(Level.FINE, "invoking createCtxNative");
 		boolean ret = createCtxNative(eventQueueSize, dataFromC);
-		logger.log(Level.INFO, "finished createCtxNative");
+		logger.log(Level.FINE, "finished createCtxNative");
 		return ret;
 		
 	}
 	private static native void closeCtxNative(long ptr);
 	public static void closeCtx(long ptr) {
-		logger.log(Level.INFO, "invoking closeCtxNative");
+		logger.log(Level.FINE, "invoking closeCtxNative");
 		closeCtxNative(ptr);
-		logger.log(Level.INFO, "finished closeCtxNative" );
+		logger.log(Level.FINE, "finished closeCtxNative" );
 			
 	}
 	private static native int runEventLoopNative(long ptr, long timeOutMicroSec);
 	public static int runEventLoop(long ptr, long timeOutMicroSec) {
-	    logger.log(Level.INFO, "invoking Bridge.runEventLoopNative");
+	    logger.log(Level.FINE, "invoking Bridge.runEventLoopNative");
 	    int ret = runEventLoopNative(ptr, timeOutMicroSec);
-	    logger.log(Level.INFO, "finished Bridge.runEventLoopNative=" + ret);
+	    logger.log(Level.FINE, "finished Bridge.runEventLoopNative=" + ret);
 	    return ret;
 	}	
 	private static native void stopEventLoopNative(long ptr);
 	public static void stopEventLoop(long ptr) {
-	    logger.log(Level.INFO, "invoking stopEventLoopNative");
+	    logger.log(Level.FINE, "invoking stopEventLoopNative");
 	    stopEventLoopNative(ptr);
-	    logger.log(Level.INFO, "finished stopEventLoopNative");
+	    logger.log(Level.FINE, "finished stopEventLoopNative");
 	}
 	private static native int addEventLoopFdNative(long ptr, long fd, int events, long priv_data);
 	public static int addEventLoopFd(long ptr, long fd, int events, long priv_data) {
-	    logger.log(Level.INFO, "invoking addEventLoopFdNative");
+	    logger.log(Level.FINE, "invoking addEventLoopFdNative");
 	    int ret = addEventLoopFdNative(ptr, fd, events, priv_data);
-	    logger.log(Level.INFO, "finished addEventLoopFdNative");
+	    logger.log(Level.FINE, "finished addEventLoopFdNative");
 	    return ret;
 	}
 	private static native int delEventLoopFdNative(long ptr, long fd);
 	public static int delEventLoopFd(long ptr, long fd) {
-	    logger.log(Level.INFO, "invoking delEventLoopFdNative");
+	    logger.log(Level.FINE, "invoking delEventLoopFdNative");
 	    int ret = delEventLoopFdNative(ptr, fd);
-	    logger.log(Level.INFO, "finished delEventLoopFdNative");
+	    logger.log(Level.FINE, "finished delEventLoopFdNative");
 	    return ret;
 	}
 
 	private static native long startSessionClientNative(String url, long ptrCtx);
 	public static long startSessionClient(String url, long ptrCtx) {
-		logger.log(Level.INFO, "invoking startSessionNative");
+		logger.log(Level.FINE, "invoking startSessionNative");
 		long p = startSessionClientNative(url, ptrCtx);		
-		logger.log(Level.INFO, "finished startSessionNative ");
+		logger.log(Level.FINE, "finished startSessionNative ");
 		return p;
 	}	
 	private static native void closeSessionClientNative(long sesPtr);
 	public static void closeSessionClient(long sesPtr) {
-		logger.log(Level.INFO, "invoking Bridge.closeSessionClient");
+		logger.log(Level.FINE, "invoking Bridge.closeSessionClient");
 		closeSessionClientNative(sesPtr);
-		logger.log(Level.INFO, "finished Bridge.closeSessionClient");
+		logger.log(Level.FINE, "finished Bridge.closeSessionClient");
 	}
 
 	private static native long [] startServerNative(String url, long ptrCtx);
 	public static long [] startServer(String url, long ptrCtx) {
-		logger.log(Level.INFO, "invoking startServerNative");
+		logger.log(Level.FINE, "invoking startServerNative");
 		long ptr [] = startServerNative(url, ptrCtx);
-		logger.log(Level.INFO, "finished startServerNative");
+		logger.log(Level.FINE, "finished startServerNative");
 		return ptr;
 	}
 	private static native boolean stopServerNative(long ptr);
 	public static boolean stopServer(long ptr) {
-		logger.log(Level.INFO, "invoking stopServerNative");
+		logger.log(Level.FINE, "invoking stopServerNative");
 		boolean ret = stopServerNative(ptr);
-		logger.log(Level.INFO, "finished stopServerNative ret=" + ret);
+		logger.log(Level.FINE, "finished stopServerNative ret=" + ret);
 		return ret;
 	}
 
 	private static native long forwardSessionNative(String url, long ptrSes, long ptrServer);
 	public static long forwardSession(String url, long ptrSes, long ptrServer) {
-		logger.log(Level.INFO, "invoking forwardSessionNative");
+		logger.log(Level.FINE, "invoking forwardSessionNative");
 		long ptr = forwardSessionNative(url, ptrSes, ptrServer);
-		logger.log(Level.INFO, "finished forwardSessionNative");
+		logger.log(Level.FINE, "finished forwardSessionNative");
 		return ptr;
 	}
 
 	private static native long createMsgPoolNative(int count, int inSize, int outSize);
 	public static long  createMsgPool(int count, int inSize, int outSize) {
-		logger.log(Level.INFO, "invoking createMsgPoolNative");
+		logger.log(Level.FINE, "invoking createMsgPoolNative");
 
 		long ptr = createMsgPoolNative(count, inSize, outSize);
-		logger.log(Level.INFO, "finished createMsgPoolNative");
+		logger.log(Level.FINE, "finished createMsgPoolNative");
 		return ptr;
 
 	}
 
 	private static native boolean sendMsgNative(long ptrSession, int sessionType, long ptrMsg);
 	public static boolean  sendMsg(long ptrSession, int sessionType, long ptrMsg) {
-		logger.log(Level.INFO, "invoking sendMsgNative");
+		logger.log(Level.FINE, "invoking sendMsgNative");
 		boolean ret = sendMsgNative(ptrSession, sessionType, ptrMsg);
-		logger.log(Level.INFO, "finished sendMsgNative ret= "+ ret);
+		logger.log(Level.FINE, "finished sendMsgNative ret= "+ ret);
 		return ret;
 	}
 
