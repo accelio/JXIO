f1b8a0ea3cdffe6303ca1cf57a8ce64308bc023c
Katya Katsenelenbogen <katyak@mellanox.com>
2013-10-10 07:59:00 +0200

    removing log prints from bridge.cc (too detailed, even for TRACE)
    Change-Id: Icbd9e37a9275d1dc58bd623cc17c6210fc95b757

--- java/src/com/mellanox/jxio/impl/Bridge.java
+++ java/src/com/mellanox/jxio/impl/Bridge.java
@@ -33,126 +33,91 @@ public class Bridge {
 	// Native methods and their wrappers start here	
 	private static native boolean createCtxNative(int eventQueueSize, Object dataFromC);
 	public static boolean createCtx(int eventQueueSize, Object dataFromC) {
-		LOG.trace("invoking createCtxNative");
 		boolean ret = createCtxNative(eventQueueSize, dataFromC);
-		LOG.trace("finished createCtxNative");
 		return ret;
 	}
 	private static native void closeCtxNative(long ptr);
 	public static void closeCtx(long ptr) {
-		LOG.trace("invoking closeCtxNative");
 		closeCtxNative(ptr);
-		LOG.trace("finished closeCtxNative" );
 	}
 	private static native int runEventLoopNative(long ptr, long timeOutMicroSec);
 	public static int runEventLoop(long ptr, long timeOutMicroSec) {
-	    LOG.trace("invoking Bridge.runEventLoopNative");
 	    int ret = runEventLoopNative(ptr, timeOutMicroSec);
-	    LOG.trace("finished Bridge.runEventLoopNative=" + ret);
 	    return ret;
 	}	
 	private static native void breakEventLoopNative(long ptr);
 	public static void breakEventLoop(long ptr) {
-	    LOG.trace("invoking breakEventLoopNative");
 	    breakEventLoopNative(ptr);
-	    LOG.trace("finished breakEventLoopNative");
 	}
 	private static native int addEventLoopFdNative(long ptr, long fd, int events, long priv_data);
 	public static int addEventLoopFd(long ptr, long fd, int events, long priv_data) {
-	    LOG.trace("invoking addEventLoopFdNative");
 	    int ret = addEventLoopFdNative(ptr, fd, events, priv_data);
-	    LOG.trace("finished addEventLoopFdNative");
 	    return ret;
 	}
 	private static native int delEventLoopFdNative(long ptr, long fd);
 	public static int delEventLoopFd(long ptr, long fd) {
-	    LOG.trace( "invoking delEventLoopFdNative");
 	    int ret = delEventLoopFdNative(ptr, fd);
-	    LOG.trace("finished delEventLoopFdNative");
 	    return ret;
 	}
 
 	private static native long startSessionClientNative(String url, long ptrCtx);
 	public static long startSessionClient(String url, long ptrCtx) {
-		LOG.trace("invoking startSessionNative");
 		long p = startSessionClientNative(url, ptrCtx);		
-		LOG.trace("finished startSessionNative ");
 		return p;
 	}	
 	private static native void closeSessionClientNative(long sesPtr);
 	public static void closeSessionClient(long sesPtr) {
-		LOG.trace("invoking Bridge.closeSessionClient");
 		closeSessionClientNative(sesPtr);
-		LOG.trace("finished Bridge.closeSessionClient");
 	}
 	
 	private static native long  startServerManagerNative(String url, long ptrCtx);
 	public static long  startServerManager(String url, long ptrCtx) {
-		LOG.trace( "invoking startServerNative");
 		long ptr  = startServerManagerNative(url, ptrCtx);
-		LOG.trace( "finished startServerNative");
 		return ptr;
 	}
 	private static native boolean stopServerManagerNative(long ptr);
 	public static boolean stopServerManager(long ptr) {
-		LOG.trace( "invoking stopServerNative");
 		boolean ret = stopServerManagerNative(ptr);
-		LOG.trace( "finished stopServerNative ret=" + ret);
 		return ret;
 	}
-	
 
 	private static native long [] startServerSessionNative(String url, long ptrCtx);
 	public static long [] startServerSession(String url, long ptrCtx) {
-		LOG.trace( "invoking startServerNative");
 		long ptr [] = startServerSessionNative(url, ptrCtx);
-		LOG.trace( "finished startServerNative");
 		return ptr;
 	}
 	private static native boolean stopServerSessionNative(long ptr);
 	public static boolean stopServerSession(long ptr) {
-		LOG.trace( "invoking stopServerNative");
 		boolean ret = stopServerSessionNative(ptr);
-		LOG.trace( "finished stopServerNative ret=" + ret);
 		return ret;
 	}
 
 	private static native long forwardSessionNative(String url, long ptrSes, long ptrServer);
 	public static long forwardSession(String url, long ptrSes, long ptrServer) {
-		LOG.trace("invoking forwardSessionNative");
 		long ptr = forwardSessionNative(url, ptrSes, ptrServer);
-		LOG.trace("finished forwardSessionNative");
 		return ptr;
 	}
 
 	private static native ByteBuffer createMsgPoolNative(int count, int inSize, int outSize, long [] ptrMsg);
 	public static ByteBuffer  createMsgPool(int count, int inSize, int outSize, long [] ptrMsg) {
-		LOG.trace("invoking createMsgPoolNative");
 		ByteBuffer b = createMsgPoolNative(count, inSize, outSize, ptrMsg);
-		LOG.trace("finished createMsgPoolNative");
 		return b;
 	}
 
 	private static native boolean sendMsgNative(long ptrSession, int sessionType, long ptrMsg);
 	public static boolean  sendMsg(long ptrSession, int sessionType, long ptrMsg) {
-		LOG.trace("invoking sendMsgNative");
 		boolean ret = sendMsgNative(ptrSession, sessionType, ptrMsg);
-		LOG.trace("finished sendMsgNative ret= "+ ret);
 		return ret;
 	}
 private static native boolean bindMsgPoolNative(long ptrMsgPool, long ptrEQH);
 	public static boolean bindMsgPool(long ptrMsgPool, long ptrEQH) {
-		LOG.trace("invoking sendMsgNative");
 		boolean ret = bindMsgPoolNative(ptrMsgPool, ptrEQH);
-		LOG.trace("finished sendMsgNative ret= "+ ret);
 		return ret;
 	}
 
 	private static native String getErrorNative(int errorReason);
 	public static String getError(int errorReason) {
-//		logger.trace("invoking getErrorNative");
 		String s = getErrorNative(errorReason);
-//		logger.trace("finished getErrorNative. error was "+s);
 		return s;
 	}
 }
