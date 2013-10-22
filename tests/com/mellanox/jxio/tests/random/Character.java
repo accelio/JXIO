3c4b7f1628809e6251e2e971dd6b629de63e8f99
Alon Grinshpoon <alongr@r-zorro010.mtr.labs.mlnx>
2013-10-22 19:24:11 +0300

    issue: 342725 Added the first updated version of the random model tests. Current version generate a a story.xml file out of a probabilty.xml file. All functions are generic. Supports randomly defined values and the use of seeds.
    Change-Id: I20e4b455c5a4e69c4a4899efb083ee1d65a839ed

---
+++ tests/com/mellanox/jxio/tests/random/Character.java
@@ -0,0 +1,103 @@
+/*
+ ** Copyright (C) 2013 Mellanox Technologies
+ **
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at:
+ **
+ ** http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ ** either express or implied. See the License for the specific language
+ ** governing permissions and  limitations under the License.
+ **
+ */
+package com.mellanox.jxio.tests.random;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Character {
+
+	private String              characterType;
+	private Map<String, String> attributes           = new HashMap<>();
+	private List<Character>     supportingCharacters = new ArrayList<>();
+
+	/**
+	 * @return The character's type
+	 */
+	public String getCharacterType() {
+		return characterType;
+	}
+
+	/**
+	 * Set the character's type to the given one.
+	 * @param characterType A string representing the character's type.
+	 */
+	public void setCharacterType(String characterType) {
+		this.characterType = characterType;
+	}
+
+	/**
+	 * Add an attribute to the character.
+	 * @param att The attribute name.
+	 * @param value The attribute value.
+	 */
+	public void setAttribute(String att, String value) {
+		attributes.put(att, value);
+	}
+	
+	/**
+	 * @param att A character attribute name.
+	 * @return The value og the given attribute.
+	 */
+	public String getAttribute(String att) {
+		return attributes.get(att);
+	}
+
+	/**
+	 * @return A Map of the character's attributes and their values.
+	 */
+	public Map<String, String> getAttributes() {
+		return this.attributes;
+	}
+
+	/**
+	 * @return A list of all the character's supporting characters.
+	 */
+	public List<Character> getSupportingCharacters() {
+		return supportingCharacters;
+	}
+
+	/**
+	 * @return A list of character types of all the character's
+	 * supporting characters.
+	 */
+	public List<String> getSupportingCharactersTypes() {
+		List<String> supportingCharactersTypes = new ArrayList<>();
+		for (Character character : supportingCharacters) {
+			supportingCharactersTypes.add(character.getCharacterType());
+		}
+		return supportingCharactersTypes;
+	}
+
+	/**
+	 * Add a a supporting character to this character.
+	 * @param supportingCharacter
+	 */
+	public void addSupportingCharacter(Character supportingCharacter) {
+		supportingCharacters.add(supportingCharacter);
+	}
+	
+	public String toString() {
+		String str = "Character Type: " + characterType.toString();
+		for (String att : attributes.keySet()) {
+			str += ", " + att + ": " + attributes.get(att);
+		}
+		return str;
+	}
+}
---
+++ tests/com/mellanox/jxio/tests/random/Main.java
@@ -0,0 +1,78 @@
+/*
+ ** Copyright (C) 2013 Mellanox Technologies
+ **
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at:
+ **
+ ** http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ ** either express or implied. See the License for the specific language
+ ** governing permissions and  limitations under the License.
+ **
+ */
+package com.mellanox.jxio.tests.random;
+
+import java.io.File;
+import java.util.Random;
+
+public class Main {
+
+	public static final int    ITERATIONS = 1;
+	public static String       xmlFileDir;
+	private static String      xmlFileName;
+	private static StoryTeller storyTeller;
+	@SuppressWarnings("unused")
+    private static long        seed;
+
+	/**
+	 * The main program that runs the probability XML file through 
+	 * the StoryTeller in order to produce a story XML file.
+	 * @param args The command line arguments.
+	 */
+	public static void main(String[] args) {
+
+		// Check for valid arguments
+		if (!argsCheck(args)) {
+			return;
+		} else {
+			for (int i = 0; i < ITERATIONS; i++) {
+				// Handle the XML file
+				xmlFileDir = args[0];
+				xmlFileName = args[1];
+				// Get input seed or randomize one
+				seed = (args.length != 3) ? new Random().nextLong() : Long.valueOf(args[2]);
+				// Create a new StoryTeller Instance
+				File storyFile = new File(xmlFileDir + "\\" + xmlFileName);
+				storyTeller = new StoryTeller(storyFile); //If needed add second argument 'seed'
+				// Tell Story
+				storyTeller.read();
+				storyTeller.write();
+			}
+		}
+	}
+
+	/**
+	 * Checks to is if the number of arguments passed is valid.
+	 * @param args The command line arguments.
+	 * @return True if number of arguments is valid.
+	 */
+	private static boolean argsCheck(String[] args) {
+		if (args.length < 2) {
+			print("[ERROR] Missing argument!\nFirst arugment needs to be the directory of the tests XML file.\nSecond arugment needs to be the file name of the tests XML file.\nA third arugment MAY be added as a seed (for random selections).");
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Prints the given message to the screen.
+	 * @param str
+	 */
+	public static void print(String str) {
+		System.out.println("\n" + str + "\n");
+	}
+}
