e34b6f6005ad819bb684cc3ccaf1a8dcfb1def98
Alon Grinshpoon <alongr@r-zorro011.mtr.labs.mlnx>
2013-11-19 19:28:24 +0200

    issue: 342723 Updated the MARS configuration files to run the random test.
    Change-Id: I1394fc63f56d8a6a77661eefcda98c2a63891d8c

--- cases/jx_wrapper.py
+++ cases/jx_wrapper.py
@@ -8,18 +8,18 @@ from reg2_wrapper.test_wrapper.client_server_wrapper import ClientServerWrapper
 class JxWrapper(ClientServerWrapper):
 
     def get_server_prog_path(self):
-        return "../accelio/tests/usr/hello_test/run_server.sh"
+        return "echo server"
 
     def get_client_prog_path(self):
-        return "../tests/runClientTest.sh"
+        return "../tests/runRandomTest.sh"
 
     def configure_parser(self):
         super(JxWrapper, self).configure_parser()
  
         # Arguments
-        self.add_client_cmd_argument('--host', help='The host ip or name of the server.', type=str, value_only=True, priority=1)
-        self.add_client_cmd_argument('--port', help='Port number of the server.', type=int, value_only=True, priority=2)
-        self.add_client_cmd_argument('--test_num', help='The test number to run.', type=int, value_only=True, priority=3)
+        self.add_client_cmd_argument('--path', help='The path to the probability file.', type=str, value_only=True, priority=1)
+        self.add_client_cmd_argument('--filename', help='The probabilty file name.', type=str, value_only=True, priority=2)
+        self.add_client_cmd_argument('--tellstory', help='YES/NO on telling a new story.', type=str, value_only=True, priority=3)
 
 #     def get_server_manage_ip(self):
 #         return self.ServerPlayer.Ip
