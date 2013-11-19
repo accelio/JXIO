d8db955c60d24b3f90eb509437f830dc1cda9508
Ori Zaig <oriz@mellanox.com>
2013-11-19 19:44:09 +0200

    issue:343357 making adaptations to support Bullseye via MARS
    Change-Id: I26299999acd7dd8ff6b1c7a18c6eef8e0d982d01

--- cases/bullseyePostHandler.sh
+++ cases/bullseyePostHandler.sh
@@ -13,11 +13,11 @@ coverageResultDir=$coverageDir/$RESULTS_DIRNAME
 
 mergedCovfileTemp=$JXIO_DIR/${MERGED_COVFILE_PREFIX}${COVFILE_SUFFIX}
 echo "$echoPrefix: merge command is: covmerge --create --file $mergedCovfileTemp $coverageResultDir/*${COVFILE_SUFFIX}"
-covmerge --create --file $mergedCovfileTemp $coverageResultDir/*${COVFILE_SUFFIX}
+$BULLSEYE_DIR/covmerge --create --file $mergedCovfileTemp $coverageResultDir/*${COVFILE_SUFFIX}
 
 codeCoverageSummaryTemp=$JXIO_DIR/$SUMMARY_FILENAME
 codeCoverageSummaryFinal=$coverageDir/$SUMMARY_FILENAME
-covdir --file $mergedCovfileTemp >> $codeCoverageSummaryTemp
+$BULLSEYE_DIR/covdir --file $mergedCovfileTemp >> $codeCoverageSummaryTemp
 
 mv $mergedCovfileTemp $coverageDir
 mv $codeCoverageSummaryTemp $coverageDir
@@ -25,7 +25,7 @@ mv $codeCoverageSummaryTemp $coverageDir
 # commiting the results
 
 echo "$echoPrefix: covselect --file $mergedCovfileTemp -i $COVERAGE_EXCLUDES"
-covselect --file $mergedCovfileTemp -i $COVERAGE_EXCLUDES
+$BULLSEYE_DIR/covselect --file $mergedCovfileTemp -i $COVERAGE_EXCLUDES
 echo "$echoPrefix: eval $CODE_COVERAGE_COMMIT_SCRIPT_PATH --branch $GIT_BRANCH --product $PRODUCT_NAME --team $TEAM_NAME --version $VERSION_SHORT_FORMAT --path $coverageResultDir"
 if [[ -z $BULLSEYE_DRYRUN ]];then
 	eval $CODE_COVERAGE_COMMIT_SCRIPT_PATH --branch $GIT_BRANCH --product $PRODUCT_NAME --team $TEAM_NAME --version $VERSION_SHORT_FORMAT --path $coverageResultDir
--- cases/copyToLocal.sh
+++ cases/copyToLocal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echoPrefix=`basename $0`
+echo "$echoPrefix: cp -r $JXIO_REMOTE_DIR/$PRODUCT_NAME/* $JXIO_DIR"
 cp -r $JXIO_REMOTE_DIR/$PRODUCT_NAME/* $JXIO_DIR
  
 
