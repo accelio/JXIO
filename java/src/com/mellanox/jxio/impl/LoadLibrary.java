ad78699552c11af5ec399c4d5edb34254a7836a0
Alex Rosenbaum <Alexr@mellanox.com>
2013-11-20 10:28:43 +0200

    issue: 352002 [Static Code Analysis] Java findbugs in LoadLibrary.java - fix 3 issues
    Make sure to handle all thrown exceptions and close InputStream/OutputStream correctly
    
    Issue were:
    1. L B OS: com.mellanox.jxio.impl.LoadLibrary.extractResource(URL) may fail to close stream on exception At LoadLibrary.java:[line 72]
    2. M X OBL: com.mellanox.jxio.impl.LoadLibrary.extractResource(URL) may fail to clean up java.io.OutputStream on checked exception Obligation to clean up resource created at LoadLibrary.java:[line 72] is not discharged
    3. M B RV: exceptional return value of java.io.File.delete() ignored in com.mellanox.jxio.impl.LoadLibrary.loadLibrary(String) At LoadLibrary.java:[line 42]
    
    Change-Id: Ic9a99fe4ec9adeec85a3960ed453dfa7600706b9

--- java/src/com/mellanox/jxio/impl/LoadLibrary.java
+++ java/src/com/mellanox/jxio/impl/LoadLibrary.java
@@ -16,69 +16,71 @@
 */
 package com.mellanox.jxio.impl;
 
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 
 public class LoadLibrary {
 
 	/**
-	 * Tries to load the library, by extracting it from the current class jar. If that fails, falls
-	 * back on {@link System#loadLibrary(String)}.
+	 * Tries to load the library, by extracting it from the current class jar. If that fails, falls back on
+	 * {@link System#loadLibrary(String)}.
 	 * 
-	 * @param String to the library name to be extracted and loaded from the jar
-	 * @return the String of the full path of the file loaded, or null on error
+	 * @param String to the library name to be extracted and loaded from the this current jar
+	 * @return true if the the file loaded successfully, or false on error
 	 */
-	public static String loadLibrary(String resourceName) {
+	public static boolean loadLibrary(String resourceName) {
 		ClassLoader loader = LoadLibrary.class.getClassLoader();
 		URL url = loader.getResource(resourceName);
+		File file = null;
 		try {
-			File file = extractResource(url);
-			if (file != null && file.exists()) {
-				String filename = file.getAbsolutePath();
+			file = extractResource(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("JXIO: Native code library failed to extract URL\n" + e);
+			return false;
+		}
 
+		if (file != null && file.exists()) {
+			String filename = file.getAbsolutePath();
+			try {
 				System.load(filename);
-				file.delete();
-				return filename;
+			} catch (UnsatisfiedLinkError e) {
+				e.printStackTrace();
+				System.err.println("JXIO: Native code library failed to load\n" + e);
+				return false;
 			}
-        } catch (IOException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-        }
-
-		String libname = url.getFile();
-		System.loadLibrary(libname);
-		return libname;
+			if (file.delete() == false) {
+				System.err.println("JXIO: Error deleting the temp native code library (filename='" + filename + "')\n");
+			}
+		}
+		return true;
 	}
 
 	/**
-	 * Extracts a resource into a temp directory. 
+	 * Extracts a resource into a temp directory.
 	 * 
 	 * @param resourceURL the URL of the resource to extract
 	 * @return the File object representing the extracted file
 	 * @throws IOException if fails to extract resource properly
 	 */
-	public static File extractResource(URL resourceURL)
-	        throws IOException {
+	public static File extractResource(URL resourceURL) throws IOException {
 		InputStream is = resourceURL.openStream();
 		if (is == null) {
 			return null;
 		}
-		File file = null;
+		File file = new File(getTempDir(), new File(resourceURL.getPath()).getName());
+		FileOutputStream os = null;
 		try {
-			file = new File(getTempDir(), new File(resourceURL.getPath()).getName());
-			FileOutputStream os = new FileOutputStream(file);
-			byte[] buffer = new byte[1024];
-			int length = 0;
-			while ((length = is.read(buffer)) != -1) {
-				os.write(buffer, 0, length);
-			}
-			is.close();
-			os.close();
-		} catch (IOException e) {
-			throw e;
+			os = new FileOutputStream(file);
+			copy(is, os);
+		} finally {
+			closeQuietly(os);
+			closeQuietly(is);
 		}
 		return file;
 	}
@@ -108,4 +110,33 @@ public class LoadLibrary {
 		return tempDir;
 	}
 
+	/**
+	 * Helper function to copy an InputStream into an OutputStream
+	 */
+	public static long copy(InputStream is, OutputStream os) throws IOException {
+		if (is == null || os == null)
+			return 0;
+		long length_total = 0;
+		byte[] buffer = new byte[1024];
+		int length = 0;
+		while ((length = is.read(buffer)) != -1) {
+			os.write(buffer, 0, length);
+			length_total += length;
+		}
+		return length_total;
+	}
+	
+	/**
+	 * Helper function to close InputStream or OutputStream in a quiet way which hides the exceptions
+	 */
+	public static void closeQuietly(Closeable c) {
+		if (c == null)
+			return;
+		try {
+			c.close();
+		} catch (IOException e) {
+			// No logging in this 'Quite Close' method
+			// System.err.println("JXIO: Failed to close '" + c + "'\n");
+		}
+	}
 }
