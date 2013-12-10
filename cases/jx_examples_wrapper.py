4dc1f657e4f8d76e9f22bd860084664a745d59a0
Alon Grinshpoon <alongr@r-zorro010.mtr.labs.mlnx>
2013-12-10 11:58:28 +0200

    issue: 358752 Added and edited the MARS configuration files.
    Change-Id: I72e36e57225351af1744bd77c8b501c00692a7de

---
+++ cases/jx_examples_wrapper.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Built-in modules
+import sys
+
+from reg2_wrapper.test_wrapper.client_server_wrapper import ClientServerWrapper
+
+class JxWrapper(ClientServerWrapper):
+
+    def get_server_prog_path(self):
+        return "../examples/runHelloWorld.sh server"
+
+    def get_client_prog_path(self):
+        return "../examples/runHelloWorld.sh client"
+
+    def configure_parser(self):
+        super(JxWrapper, self).configure_parser()
+ 
+        # Arguments
+        self.add_client_cmd_argument('--ip', help='The server's IP.', type=str, value_only=True, priority=1)
+        self.add_client_cmd_argument('--port', help='The server's port', type=str, value_only=True, priority=2)
+
+        self.add_server_cmd_argument('--ip', help='The server's IP.', type=str, value_only=True, priority=1)
+        self.add_server_cmd_argument('--port', help='The server's port', type=str, value_only=True, priority=2)
+
+#     def get_server_manage_ip(self):
+#         return self.ServerPlayer.Ip
+# 
+#     def get_client_manage_ip(self):
+#         return self.ClientPlayers[0].Ip
+# 
+#     def get_client_test_ip(self):
+#         return self.ClientEPoints[0].ipv4
+                              
+if __name__ == "__main__":
+    wrapper = JxWrapper("JX Wrapper")
+    wrapper.execute(sys.argv[1:])
+
