ec666a3e709eb6fb850494dc78779ef9ab933a81
Alon Grinshpoon <alongr@r-zorro010.mtr.labs.mlnx>
2013-12-22 15:07:46 +0200

    issue: 360513 Updated MARS wrapper, cases file and probability file to support tests on two machines in parallel.
    Change-Id: Ic8bdee354314d8231318e3668fd9325ad571eda5

--- cases/jx_random_wrapper.py
+++ cases/jx_random_wrapper.py
@@ -3,36 +3,23 @@
 # Built-in modules
 import sys
 
-from reg2_wrapper.test_wrapper.client_server_wrapper import ClientServerWrapper
+from reg2_wrapper.test_wrapper.standalone_wrapper import StandaloneWrapper
+from reg2_wrapper.utils.parser.cmd_argument import RunningStage
 
-class JxWrapper(ClientServerWrapper):
+class JxRandomWrapper(StandaloneWrapper):
 
-    def get_server_prog_path(self):
-        return "echo server"
-
-    def get_client_prog_path(self):
+    def get_prog_path(self, running_stage=RunningStage.RUN):
         return "../tests/runRandomTest.sh"
 
     def configure_parser(self):
-        super(JxWrapper, self).configure_parser()
- 
+        super(JxRandomWrapper, self).configure_parser()
+
         # Arguments
-        self.add_client_cmd_argument('--path', help='The path to the probability file.', type=str, value_only=True, priority=1)
-        self.add_client_cmd_argument('--filename', help='The probabilty file name.', type=str, value_only=True, priority=2)
-        self.add_client_cmd_argument('--random_seed', help='The fixed random seed. Set to 0 to randimize a seed.', type=str, value_only=True, priority=3)
-        self.add_client_cmd_argument('--timeout', help='The timeout in seconds for the entire run.', type=str, value_only=True, priority=4)
+        self.add_cmd_argument('--path', help='The path to the probability file.', type=str, value_only=True, priority=1, action='store', required=True)
+        self.add_cmd_argument('--filename', help='The probabilty file name.', type=str, value_only=True, priority=2, action='store', required=True)
+	self.add_test_attribute_argument('', 'seed', priority=3)
+        self.add_cmd_argument('--timeout', help='The timeout in seconds for the entire run.', type=str, value_only=True, priority=4, action='store', required=True)
 
-
-#     def get_server_manage_ip(self):
-#         return self.ServerPlayer.Ip
-# 
-#     def get_client_manage_ip(self):
-#         return self.ClientPlayers[0].Ip
-# 
-#     def get_client_test_ip(self):
-#         return self.ClientEPoints[0].ipv4
-                              
 if __name__ == "__main__":
-    wrapper = JxWrapper("JX Wrapper")
+    wrapper = JxRandomWrapper("JX Random Wrapper")
     wrapper.execute(sys.argv[1:])
-
