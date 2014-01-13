74c0a94dbe415eb8a3a575fce1bd73b9bfe31a2a
Katya Katsenelenbogen <katyak@mellanox.com>
2014-01-13 11:29:42 +0200

    issue: 350325 adding test case to BreakEventLoopTests - checking timeout when receiving an event
    Change-Id: Id2ed51b5d27becc621d8fee944778f1ba213b996

--- tests/com/mellanox/jxio/tests/BreakEventLoopTests.java
+++ tests/com/mellanox/jxio/tests/BreakEventLoopTests.java
@@ -16,37 +16,44 @@
  */
 package com.mellanox.jxio.tests;
 
-import com.mellanox.jxio.EventQueueHandler;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class BreakEventLoopTests implements Runnable {
+import com.mellanox.jxio.ClientSession;
+import com.mellanox.jxio.EventName;
+import com.mellanox.jxio.EventReason;
+import com.mellanox.jxio.EventQueueHandler;
+import com.mellanox.jxio.Msg;
+
+public class BreakEventLoopTests{
 
 	public class InnerEQHThread implements Runnable {
 
 		private volatile boolean exitFlag = false;
-		public int loops = 0;
-		public int wakeups = 0;
-		EventQueueHandler eqh;
-		
+		public int               loops    = 0;
+		public int               wakeups  = 0;
+		EventQueueHandler        eqh;
+
 		public void run() {
 			print("----- Setting up a event queue handler...");
 			eqh = new EventQueueHandler(null);
-			
+
 			checkEQHBreakFromOtherThread();
 			checkEQHBreakFromSelfThread();
-			checkEQHTimeout(10000);    //    10 msec
-			checkEQHTimeout(100000);   //   100 msec
-			checkEQHTimeout(1000000);  //  1000 msec (1 sec)
-			checkEQHTimeout(500000);   //   500 msec
-			checkEQHTimeout(2000000);  //  2     sec
-			checkEQHTimeout(4000000);  //  4     sec
-			checkEQHTimeout(300000);   //   300 msec
-			checkEQHTimeout(12000000); // 12     sec
-			
+
+			// this array holds the durations of the timeout (in millisec). 
+			final int[] durationsArray = { 10, 100, 300, 500, 1000, 2000, 4000 };
+			for (int time : durationsArray) {
+				//checkEQHTimeOut and checkEQHTimeoutWithEvent accept time in usec
+				checkEQHTimeout(time * 1000);
+				checkEQHTimeoutWithEvent(time * 1000);
+			}
+
 			print("----- Closing the event queue handler...");
 			eqh.close();
 			print("----- Exiting up a event queue handler...");
 		}
-		
+
 		public void stop() {
 			print("----- Stopping running thread with event loop");
 			exitFlag = true;
@@ -62,7 +69,7 @@ public class BreakEventLoopTests implements Runnable {
 
 		private void blockOnEQH() {
 			print("----- Continue event loop in blocking mode...");
-			eqh.runEventLoop(1, -1); // Blocking! No events should trigger exit from loop unless broken on purpose 
+			eqh.runEventLoop(1, -1); // Blocking! No events should trigger exit from loop unless broken on purpose
 			loops++; // count the number of loops
 			print("----- Got out of event loop...(loops = " + loops + ")");
 		}
@@ -81,20 +88,44 @@ public class BreakEventLoopTests implements Runnable {
 		}
 
 		private void checkEQHTimeout(long timeOutUSec) {
-			print("--- Testing EQH (" + timeOutUSec/1000 + " msec sleep)...");
-			long start = System.nanoTime()/1000;
+			print("--- Testing EQH (" + timeOutUSec / 1000.0 + " msec sleep)...");
+			long start = System.nanoTime() / 1000;
 			eqh.runEventLoop(1, timeOutUSec); // Blocking with Timeout!
-			long duration = System.nanoTime()/1000 - start;
+			long duration = System.nanoTime() / 1000 - start;
 			long delta = duration - timeOutUSec;
 			long abs_delta = (delta < 0) ? -delta : delta;
 			// event_loop epoll_wait() sleep accuracy is dependent on requested timeout period (about 0,1%)
-			if ((abs_delta > 500) && (timeOutUSec/800 < abs_delta)) {
-				printFailureAndExit("(it took too much time to wake up from EQH (blocked for " + delta + " usec more then requested)");
-			} 
-			else {
-				print("----- Woken by timeout correctly after " + timeOutUSec/1000 + " msec (with " + delta + " usec offset) ...");
+			if ((abs_delta > 500) && (timeOutUSec / 800 < abs_delta)) {
+				printFailureAndExit("(it took too much time to wake up from EQH (blocked for " + delta
+				        + " usec more then requested)");
+			} else {
+				print("----- Woken by timeout correctly after " + timeOutUSec / 1000 + " msec (with " + delta
+				        + " usec offset) ...");
 			}
-		}	
+		}
+
+		private void checkEQHTimeoutWithEvent(long timeOutUSec) {
+			print("--- Testing EQH (" + timeOutUSec / 1000.0 + " msec sleep) with event...");
+			ClientSession c = createDummyClient(eqh);
+			long start = System.nanoTime() / 1000;
+			eqh.runEventLoop(-1, timeOutUSec); // Blocking with Timeout!
+			long duration = System.nanoTime() / 1000 - start;
+			long delta = duration - timeOutUSec;
+			long abs_delta = (delta < 0) ? -delta : delta;	
+			//client should be closing, since he received event SessionClosed
+			if (!c.getIsClosing()){
+				c.close();		
+			}
+			// event_loop epoll_wait() sleep accuracy is dependent on requested timeout period (about 0,1%)
+			// each ascent from C to Java costs ~1 milli sec
+			if ((abs_delta > 2000) && (timeOutUSec / 800 < abs_delta)) {
+				printFailureAndExit("(it took too much time to wake up from EQH with event (blocked for " + delta
+				        + " usec more then requested)");
+			} else {
+				print("----- Woken by timeout correctly after " + timeOutUSec / 1000 + " msec (with " + delta
+				        + " usec offset) ...");
+			}
+		}
 	}
 
 	public void run() {
@@ -105,7 +136,7 @@ public class BreakEventLoopTests implements Runnable {
 		InnerEQHThread eqh1 = new InnerEQHThread();
 		Thread t1 = new Thread(eqh1);
 		t1.start();
-		
+
 		try {
 			Thread.sleep(2000);
 
@@ -122,7 +153,7 @@ public class BreakEventLoopTests implements Runnable {
 			Thread.sleep(1000);
 
 			eqh1.stop();
-			
+
 			// Wait for thread to end (while it is running some 'timeout' checks)
 			t1.join();
 		} catch (InterruptedException e1) {
@@ -130,17 +161,44 @@ public class BreakEventLoopTests implements Runnable {
 		}
 
 		if (eqh1.loops != eqh1.wakeups) {
-			printFailureAndExit("wrong number of wakeup times (internal thread="+eqh1.loops+", wakeup called="+eqh1.wakeups+")");
+			printFailureAndExit("wrong number of wakeup times (internal thread=" + eqh1.loops + ", wakeup called="
+			        + eqh1.wakeups + ")");
 		} else {
 			System.out.println("*** Test Passed! *** ");
 		}
 	}
-	
+
+	private ClientSession createDummyClient(EventQueueHandler eqh) {
+		URI uri = null;
+		try {
+			uri = new URI("rdma://0.0.0.0:1234");
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			return null;
+		}
+		ClientSession client = new ClientSession(eqh, uri, new DummyCallbacks());
+		return client;
+	}
+
+	class DummyCallbacks implements ClientSession.Callbacks {
+		public void onSessionEstablished() {
+		}
+
+		public void onReply(Msg msg) {
+		}
+
+		public void onSessionEvent(EventName session_event, EventReason reason) {
+		}
+
+		public void onMsgError() {
+		}
+	}
+
 	public static void main(String[] args) {
-	    BreakEventLoopTests test = new BreakEventLoopTests();
-	    test.run();
-    }
-	
+		BreakEventLoopTests test = new BreakEventLoopTests();
+		test.run();
+	}
+
 	private void print(String str) {
 		System.out.println("[tid=" + Thread.currentThread().getId() + "] " + str);
 	}
