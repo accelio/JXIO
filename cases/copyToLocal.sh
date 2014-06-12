fa38f6a680458956eabc27821f6f0278e1332c7e
Katya Katsenelenbogen <katyak@mellanox.com>
2014-06-12 07:19:13 +0300

    issue: 396062 removing cases dir (now located only in jxio-regression repository)
    Change-Id: Ib6a1a16e59a63396eb1144406dcfe575573b23d4

--- cases/copyToLocal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echoPrefix=`basename $0`
-echo "$echoPrefix: cp -r $JXIO_REMOTE_DIR/$PRODUCT_NAME/* $JXIO_DIR"
-cp -r $JXIO_REMOTE_DIR/$PRODUCT_NAME/* $JXIO_DIR
- 
-
-
--- cases/storeProject.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echoPrefix=`basename $0`
-
-projectRemoteDir=$JXIO_REMOTE_DIR/$PRODUCT_NAME
-echo "$echoPrefix: sudo rm -rf $projectRemoteDir"
-sudo rm -rf $projectRemoteDir
-echo "$echoPrefix: cp -r $JXIO_DIR $projectRemoteDir"
-cp -r $JXIO_DIR $projectRemoteDir
